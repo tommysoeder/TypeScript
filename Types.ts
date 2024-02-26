c1b3b0ac786ef266f573d1a69fbf39eddcc1646d
tommysoeder <148865051+tommysoeder@users.noreply.github.com>
2024-02-26 11:51:49 +0100

    Add files via upload

---
+++ Types.ts
@@ -0,0 +1,32 @@
+/**
+ * START: Follow the instructions below.
+ */
+
+// Add type annotations to each of these variables.
+
+let country = "Italy";
+//^? let country:string
+
+let year = undefined;
+//^? let year:any
+
+let averageTemperature = 23.6;
+//^? laverageTemperature:number
+
+let visited = true;
+//^? let visited:boolean
+
+let currency = null;
+//^? let currency:any
+
+// Change the type annotations on these variables so they are correct.
+
+let population: number = 47_450_795;
+
+let isSummer: boolean = false;
+
+let languages: string = "Spanish, English";
+
+// ----
+
+export {};
